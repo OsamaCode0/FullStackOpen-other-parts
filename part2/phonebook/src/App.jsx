d42365f14bf739855d9f00677aec74cbb349798e
Daniil <161656537+OsamaCode0@users.noreply.github.com>
2024-09-08 03:24:14 +0300

    Update App.jsx

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
 import Filter from './components/Filter';
-import PersonForm from './components/PersonForm'; 
-import Persons from './components/Persons';     
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
 
 
 
@@ -10,33 +10,33 @@ import Persons from './components/Persons';
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456' }]);
-  const [newName, setNewName] = useState('');
-  const [newPhone, setNewPhone] = useState('');
-  const [filter, setFilter] = useState(''); 
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addNote = (event) => {
     event.preventDefault();
 
     if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to the phonebook`);
+      alert(`${newName} is already added to the phonebook`)
       return;
     }
 
-    setPersons(persons.concat({ name: newName, number: newPhone }));
-    setNewName('');
-    setNewPhone('');
+    setPersons(persons.concat({ name: newName, number: newPhone }))
+    setNewName('')
+    setNewPhone('')
   };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
+    setNewName(event.target.value)
   };
 
   const handlePhoneChange = (event) => {
-    setNewPhone(event.target.value);
+    setNewPhone(event.target.value)
   };
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value);
+    setFilter(event.target.value)
   };
 
   const personsToShow = filter
